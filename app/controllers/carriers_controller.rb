2eccdac496f656178782e9affa7361920315a907
Allan da Silva <allandasilva.dev@gmail.com>
2022-05-18 14:27:49 -0300

    Usuário cadastrar transportadora

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -6,4 +6,21 @@ class CarriersController < ApplicationController
 	def show
 		@carrier = Carrier.find(params[:id])
 	end
+
+	def new
+		@carrier = Carrier.new
+	end
+
+	def create
+		# strong parameters
+		carrier_params = params.require(:carrier).permit(:corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email_domain, :activated)
+		@carrier = Carrier.new(carrier_params)
+		if @carrier.save()
+			flash[:notice] = 'Transportadora cadastrada com sucesso.'
+			redirect_to carrier_path( @carrier.id )
+		else
+			flash.now[:notice] = 'Transportadora não cadastrada.'
+			render 'new'
+		end
+	end
 end
---
+++ spec/system/carriers/user_register_carrier_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra uma transportadora' do
+	it 'a partir da tela de listagem das transportadoras' do
+		# Arrange
+
+		# Act
+		visit root_path
+		within('nav') do
+			click_on 'Transportadoras'
+		end
+		click_on 'Cadastrar Transportadora'
+
+		# Assert
+		# verifica se todos os campos necessarios existem.
+		expect(page).to have_field('Razão social')
+		expect(page).to have_field('Nome fantasia')
+		expect(page).to have_field('CNPJ')
+		expect(page).to have_field('Endereço')
+		expect(page).to have_field('Cidade')
+		expect(page).to have_field('Estado')
+		expect(page).to have_field('Domínio')
+		expect(page).to have_field('Status')
+	end
+
+	it 'com sucesso' do
+		# Arrange
+
+		# Act
+		visit root_path
+		within('nav') do
+			click_on 'Transportadoras'
+		end
+		click_on 'Cadastrar Transportadora'
+		fill_in 'Razão social', with: 'ACME LTDA'
+		fill_in 'Nome fantasia', with: 'ACME'
+		fill_in 'CNPJ', with: '12242556123254'
+		fill_in 'Endereço', with: 'Rua das Flores, 200'
+		fill_in 'Cidade', with: 'Bauru'
+		fill_in 'Estado', with: 'SP'
+		fill_in 'Domínio', with: 'acme.com.br'
+		select 'Activated', from: 'Status'
+		click_on 'Enviar'
+
+		# Assert
+		expect(page).to have_content 'Transportadora cadastrada com sucesso.'
+		expect(page).to have_content 'ACME LTDA'
+		expect(page).to have_content '12242556123254'
+		expect(page).to have_content 'acme.com.br'
+	end
+
+	it 'com dados imcompletos' do
+		# Arrange
+
+		# Act
+		visit root_path
+		within('nav') do
+			click_on 'Transportadoras'
+		end
+		click_on 'Cadastrar Transportadora'
+		fill_in 'Razão social', with: ''
+		fill_in 'CNPJ', with: ''
+		fill_in 'Domínio', with: ''
+		click_on 'Enviar'
+
+		# Assert
+		expect(page).to have_content 'Transportadora não cadastrada.'
+		expect(page).to have_content 'Razão social não pode ficar em branco'
+		expect(page).to have_content 'CNPJ não pode ficar em branco'
+	end
+
+	it 'ou volta para listagem de transportadoras' do
+		# Arrange
+		# Act
+		visit root_path
+		within('nav') do
+			click_on 'Transportadoras'
+		end
+		click_on 'Cadastrar Transportadora'
+		click_on 'Voltar'
+
+		# Assert
+		expect(current_path).to eq carriers_path
+	end
+end
