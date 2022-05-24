a43ed0ac55cb651e60d2a4f76827b2fbbe6d9b24
Allan da Silva <allandasilva.dev@gmail.com>
2022-05-24 08:26:45 -0300

    Cria o login e o logout do sistema.

---
+++ spec/system/auth/user_sign_in_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+	it 'com sucesso' do
+		# Arrange
+		User.create!(email: 'joao@email.com', password: '123456')
+
+		# Act
+		visit root_path
+		within('nav') do
+			click_on 'Entrar'
+		end
+		within('form') do
+			fill_in 'E-mail', with: 'joao@email.com'
+			fill_in 'Senha', with: '123456'
+			click_on 'Entrar'
+		end
+
+		# Assert
+		expect(page).to have_content 'Login efetuado com sucesso.'
+		expect(page).not_to have_link 'Entrar'
+		expect(page).to have_button 'Sair'
+		within('nav') do
+			expect(page).to have_content 'joao@email.com'
+		end
+	end
+
+	it 'e faz logout' do
+		# Arrange
+		User.create!(email: 'joao@email.com', password: '123456')
+
+		# Act
+		visit root_path
+		within('nav') do
+			click_on 'Entrar'
+		end
+		within('form') do
+			fill_in 'E-mail', with: 'joao@email.com'
+			fill_in 'Senha', with: '123456'
+			click_on 'Entrar'
+		end
+		within('nav') do
+			click_on 'Sair'
+		end
+
+		# Assert
+		expect(page).to have_content 'Logout efetuado com sucesso.'
+		expect(page).to have_link 'Entrar'
+		expect(page).not_to have_button 'Sair'
+		expect(page).not_to have_content 'joao@email.com'
+	end
+end
